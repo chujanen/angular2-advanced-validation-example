56df662fb96122ef4a881b77eebc6a77a13fb4c3
Christopher Hujanen <chujanen@softmill.com>
2016-08-02 15:58:38 -0500

    forms approach WIP

--- src/app/forms-child1/forms-child1.component.ts
+++ src/app/forms-child1/forms-child1.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {REACTIVE_FORM_DIRECTIVES, FormControl, FormGroup} from '@angular/forms';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-forms-child1',
   templateUrl: 'forms-child1.component.html',
-  styleUrls: ['forms-child1.component.css']
+  styleUrls: ['forms-child1.component.css'],
+  directives: [REACTIVE_FORM_DIRECTIVES]
 })
 export class FormsChild1Component implements OnInit {
 
--- src/app/forms-child2/forms-child2.component.ts
+++ src/app/forms-child2/forms-child2.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {REACTIVE_FORM_DIRECTIVES, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'app-forms-child2',
   templateUrl: 'forms-child2.component.html',
-  styleUrls: ['forms-child2.component.css']
+  styleUrls: ['forms-child2.component.css'],
+  directives: [REACTIVE_FORM_DIRECTIVES]
 })
 export class FormsChild2Component implements OnInit {
+
+  possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  // possibleNumbers
 }
