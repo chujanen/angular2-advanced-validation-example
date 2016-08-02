26afe6c2a1d9b233ac83a909e9b85dfae12976b7
sohinderjit.singh@gmail.com <sohinderjit.singh@gmail.com>
2016-08-02 15:43:54 -0500

    added model child 1

---
+++ src/app/model-child1/index.ts
@@ -0,0 +1,1 @@
+export * from './model-child1.component';
---
+++ src/app/model-child1/model-child1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-model-child1',
+  templateUrl: 'model-child1.component.html',
+  styleUrls: ['model-child1.component.css']
+})
+export class ModelChild1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
